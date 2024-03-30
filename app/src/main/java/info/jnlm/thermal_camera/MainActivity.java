98ee20795d538edb2e3fcf013201c1fb60bf720c
jnlm <j@jnlm.info>
2024-03-30 01:25:36 -0700

    Minimal app

---
+++ app/src/main/java/info/jnlm/thermal_camera/MainActivity.java
@@ -0,0 +1,17 @@
+package info.jnlm.thermal_camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        TextView textView = new TextView(this);
+        textView.setText("Hello, World!");
+        
+        setContentView(textView);
+    }
+}
