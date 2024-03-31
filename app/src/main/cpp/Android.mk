595fa15ea058a2a7dafb82c610174e51a3ad049c
jnlm <j@jnlm.info>
2024-03-31 09:38:50 -0700

    Added more libraries

---
+++ app/src/main/cpp/Android.mk
@@ -0,0 +1,74 @@
+
+MK_LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(MK_LOCAL_PATH)
+include $(LOCAL_PATH)/libyuv/Android.mk
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(MK_LOCAL_PATH)
+USE_PC_NAME := 1
+include $(LOCAL_PATH)/libusb/android/jni/libusb.mk
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(MK_LOCAL_PATH)
+LOCAL_MODULE := libjpeg-turbo
+LOCAL_SRC_FILES := $(LOCAL_PATH)/libjpeg-turbo/build/libturbojpeg.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libjpeg-turbo
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/libjpeg-turbo/build
+LOCAL_EXPORT_LDLIBS := -llog
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(MK_LOCAL_PATH)
+LOCAL_MODULE := libjpeg
+LOCAL_SRC_FILES := $(LOCAL_PATH)/libjpeg-turbo/build/libjpeg.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/libjpeg-turbo
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/libjpeg-turbo/build
+LOCAL_EXPORT_LDLIBS := -llog
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(MK_LOCAL_PATH)/libuvc
+UVC_ROOT_REL := .
+UVC_ROOT_ABS := $(LOCAL_PATH)
+
+LOCAL_SRC_FILES := \
+    $(UVC_ROOT_REL)/src/ctrl.c \
+    $(UVC_ROOT_REL)/src/device.c \
+    $(UVC_ROOT_REL)/src/diag.c \
+    $(UVC_ROOT_REL)/src/frame.c \
+    $(UVC_ROOT_REL)/src/frame-mjpeg.c \
+    $(UVC_ROOT_REL)/src/init.c \
+    $(UVC_ROOT_REL)/src/stream.c
+
+
+LOCAL_C_INCLUDES += \
+    $(UVC_ROOT_ABS)/.. \
+    $(UVC_ROOT_ABS)/include \
+    $(UVC_ROOT_ABS)/include/libuvc
+
+LOCAL_EXPORT_C_INCLUDES := \
+    $(UVC_ROOT_ABS)/ \
+    $(UVC_ROOT_ABS)/include \
+    $(UVC_ROOT_ABS)/include/libuvc
+
+LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%)
+LOCAL_CFLAGS += -DANDROID_NDK
+LOCAL_CFLAGS += -DLOG_NDEBUG
+LOCAL_CFLAGS += -DUVC_DEBUGGING
+
+LOCAL_EXPORT_LDLIBS := -llog
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SHARED_LIBRARIES += usb-1.0
+LOCAL_SHARED_LIBRARIES += libjpeg-turbo
+LOCAL_SHARED_LIBRARIES += libjpeg
+
+$(warning ###################################################################################)
+$(warning $(LOCAL_SRC_FILES))
+$(warning $(LOCAL_C_INCLUDES))
+
+LOCAL_MODULE := libuvc
+include $(BUILD_SHARED_LIBRARY)
--- app/src/main/java/info/jnlm/thermal_camera/MainActivity.java
+++ app/src/main/java/info/jnlm/thermal_camera/MainActivity.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	static {
+		System.loadLibrary("usb-1.0");
+		System.loadLibrary("jpeg");
+		System.loadLibrary("uvc");
+	}
+    public native long nativeCreate( long camera_pointer);
+
+    private long mNativePtr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        if (mNativePtr == 0) {
+           mNativePtr = nativeCreate(mNativePtr);
+		}
         TextView textView = new TextView(this);
-        textView.setText("Hello, World!");
+        textView.setText("Hello, World!" + mNativePtr);
         
         setContentView(textView);
     }
